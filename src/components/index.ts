a8f8b87d02104aa6d66e8290cf97f96ff0e7ba3a
junaama <0naama0@gmail.com>
2025-12-17 01:34:44 -0500

    clean

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,3 @@ export { LoadingBar } from './LoadingBar';
 export { PerfumePentagram, createPentagramPoints } from './PerfumePentagram';
 export { ScreenWrapper } from './ScreenWrapper';
 export { ScentShareCard } from './ScentShareCard';
-export { ShareableCard } from './ShareableCard';
-export { SwipeableScreen } from './SwipeableScreen';
